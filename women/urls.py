2f20d538e66b910e7317e221639e9eff225c953d
Dmitry Zagorevskiy <7281015@gmail.com>
2021-12-10 12:33:41 +0100

    9. Creating links between models through the ForeignKey class.

--- women/urls.py
+++ women/urls.py
@@ -6,7 +6,12 @@ urlpatterns = [
     path('', index, name='home'),
     path('about/', about, name='about'),
     # int str slug uuid path
-    path('cats_old/', categories_old, name='categories_old'),  # http://127.0.0.1:8000/cats_old/
-    path('cats/<int:catid>/', categories, name='categories'),  # http://127.0.0.1:8000/cats/1/
-    re_path(r'^archive/(?P<year>[0-9]{4})/', archive, name='archive'),  # http://127.0.0.1:8000/cats/
+    # path('cats_old/', categories_old, name='categories_old'),  # http://127.0.0.1:8000/cats_old/
+    # path('cats/<int:catid>/', categories, name='categories'),  # http://127.0.0.1:8000/cats/1/
+    # re_path(r'^archive/(?P<year>[0-9]{4})/', archive, name='archive'),  # http://127.0.0.1:8000/cats/
+    path('addpage/', addpage, name='add_page'),
+    path('contact/', contact, name='contact'),
+    path('login/', login, name='login'),
+    path('post/<int:post_id>/', show_post, name='post'),
+    path('category/<int:cat_id>/', show_category, name='category'),
 ]
--- women/views.py
+++ women/views.py
@@ -5,39 +5,91 @@ from django.http import HttpResponse, HttpResponseNotFound, Http404
 from .models import *
 
 
-menu = ["О сайте", "Добавить статью", "Обратная связь", "Войти"]
+menu = [
+    {'title': "О сайте", 'url_name': 'about'},
+    {'title': "Добавить статью", 'url_name': 'add_page'},
+    {'title': "Обратная связь", 'url_name': 'contact'},
+    {'title': "Войти", 'url_name': 'login'}
+]
 
 
 # def index(request):
 #     return HttpResponse("Страница приложения women.")
 def index(request):
     posts = Women.objects.all()
-    return render(request, 'women/index.html', {'posts': posts, 'menu': menu, 'title': 'Главная страница'})
+    cats = Category.objects.all()
+
+    context = {
+        'posts': posts,
+        'cats': cats,
+        'menu': menu,
+        'title': 'Главная страница',
+        'cat_selected': 0,
+    }
+    return render(request, 'women/index.html', context=context)
 
 
 def about(request):
     return render(request, 'women/about.html', {'menu': menu, 'title': 'О сайте'})
 
 
-def categories_old(request):
-    return HttpResponse("<h1>Статьи по категориям OLD</h1>")
+# def categories_old(request):
+#     return HttpResponse("<h1>Статьи по категориям OLD</h1>")
+#
+#
+# def categories(request, catid):
+#     if request.GET:
+#         print(request.GET)
+#     return HttpResponse(f"<h1>Статьи по категориям</h1><p>{catid}</p>")
+#
+#
+# def archive(request, year):
+#     if int(year) > 2020:
+#         # return redirect("/")  # 302 временный
+#         # return redirect("/", permanent=True)  # 301 постоянный
+#         # home - имя из women/urls.py urlpatterns
+#         return redirect("home", permanent=True)  # 301 постоянный
+#
+#     return HttpResponse(f"<h1>Архив по категориям</h1><p>{year}</p>")
 
 
-def categories(request, catid):
-    if request.GET:
-        print(request.GET)
-    return HttpResponse(f"<h1>Статьи по категориям</h1><p>{catid}</p>")
+def about(request):
+    return render(request, 'women/about.html',
+                  {'menu': menu, 'title': 'О сайте'})
 
 
-def archive(request, year):
-    if int(year) > 2020:
-        # return redirect("/")  # 302 временный
-        # return redirect("/", permanent=True)  # 301 постоянный
-        # home - имя из women/urls.py urlpatterns
-        return redirect("home", permanent=True)  # 301 постоянный
+def addpage(request):
+    return HttpResponse("Добавление статьи")
 
-    return HttpResponse(f"<h1>Архив по категориям</h1><p>{year}</p>")
+
+def contact(request):
+    return HttpResponse("Обратная связь")
+
+
+def login(request):
+    return HttpResponse("Авторизация")
+
+
+def show_post(request, post_id):
+    return HttpResponse(f"Отображение статьи с id = {post_id}")
 
 
 def pageNotFound(request, exception):
     return HttpResponseNotFound("<h1>Страница не найдена</h1>")
+
+
+def show_category(request, cat_id):
+    posts = Women.objects.filter(cat_id=cat_id)
+    if len(posts) == 0:
+        raise Http404()
+    cats = Category.objects.all()
+
+    context = {
+        'posts': posts,
+        'cats': cats,
+        'menu': menu,
+        'title': 'Отображение по рубрикам',
+        'cat_selected': cat_id,
+    }
+
+    return render(request, 'women/index.html', context=context)
